0b9c7da6df5bd7204e91c6e3614d4e398b80bad2
Galkin Aleksey <galkin_aleksey@inbox.ru>
2023-10-14 22:13:57 +0300

    Добавлены резалт модель и резалт представление

---
+++ Models/ResultProfission.cs
@@ -0,0 +1,18 @@
+﻿using YourProfessionWebApp.Domain.Entities;
+using YourProfessionWebApp.Domain.Repositories.Interfaces;
+
+namespace YourProfessionWebApp.Models {
+    public class ResultProfission {
+        private readonly IProfessionItemRepository professionItemRepository;
+        private readonly List<ProfessionItem> professions;
+
+        public ResultProfission(IProfessionItemRepository professionItemRepository, List<Interest> interests) {
+            this.professionItemRepository = professionItemRepository;
+            professions = professionItemRepository.GetAllProfessionItems().ToList();
+        }
+
+        /*public ProfessionItem GetBestProfession(List<Interest> interests) {
+
+        }*/
+    }
+}
